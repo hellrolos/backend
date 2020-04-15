3a51ac7262b14e048ef3ec6ee7129bbbc7f91f75
ccomputo <ccomputo@ittepic.edu.mx>
2020-04-15 14:30:13 -0600

    Commit antes de subir a Github

--- config/app.php
+++ config/app.php
@@ -174,6 +174,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        Irazasyed\JwtAuthGuard\JwtAuthGuardServiceProvider::class,
 
     ],
 
@@ -207,6 +208,8 @@ return [
         'File' => Illuminate\Support\Facades\File::class,
         'Gate' => Illuminate\Support\Facades\Gate::class,
         'Hash' => Illuminate\Support\Facades\Hash::class,
+        'JWTAuth' => 'Tymon\JWTAuth\Facades\JWTAuth',
+        'JWTFactory' => 'Tymon\JWTAuth\Facades\JWTFactory',
         'Lang' => Illuminate\Support\Facades\Lang::class,
         'Log' => Illuminate\Support\Facades\Log::class,
         'Mail' => Illuminate\Support\Facades\Mail::class,
---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+use App\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'Administrador',
+            'email' => 'admin@gmail.com',
+            'password' => bcrypt('secret'),
+        ]);
+    }
+}
